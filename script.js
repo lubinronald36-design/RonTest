e1f671d33958512cac7bd75f4f344d4581c59af5
lubinronald36-design <lubinronald36@gmail.com>
2025-11-14 12:03:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let time = 60;
 let timer = setInterval(() => {
   time--;
@@ -15,30 +16,61 @@ const questions = [
   {
     question: "If I __ (study) harder, I would pass the exam.",
     answerId: "answer1",
-    correctAnswer: "studied"
+    correctAnswer: "studied",
   },
   {
     question: "What's the difference between \"affect\" and \"effect\"?",
     answerId: "answer2",
-    correctAnswer: "Affect is a verb, effect is a noun"
+    correctAnswer: "Affect is a verb, effect is a noun",
   },
   {
     question: "The book __ (write) by my favorite author.",
     answerId: "answer3",
-    correctAnswer: "was written"
+    correctAnswer: "was written",
   },
   {
     question: "If it __ (rain), I would take an umbrella.",
     answerId: "answer4",
-    correctAnswer: "rained"
+    correctAnswer: "rained",
   },
   {
     question: "The company __ (expand) its operations to five countries.",
     answerId: "answer5",
-    correctAnswer: "has expanded"
-  }
+    correctAnswer: "has expanded",
+  },
 ];
 
 const questionsDiv = document.getElementById("questions");
 
-questions
+// Function to render questions
+function renderQuestions() {
+  questions.forEach((q, index) => {
+    const questionHtml = `
+      <p>${q.question}</p>
+      <input type="text" id="${q.answerId}" placeholder="Your answer">
+      <button onclick="checkAnswer(${index})">Submit</button>
+    `;
+    questionsDiv.innerHTML += questionHtml;
+  });
+}
+
+// Function to check answers
+function checkAnswer(index) {
+  const userAnswer = document.getElementById(questions[index].answerId).value;
+  if (userAnswer.toLowerCase() === questions[index].correctAnswer.toLowerCase()) {
+    correctCount++;
+    document.getElementById("correct").innerHTML = correctCount;
+  } else {
+    incorrectCount++;
+    document.getElementById("incorrect").innerHTML = incorrectCount;
+  }
+}
+
+// Render questions on page load
+renderQuestions();
+
+
+You'll also need to add the checkAnswer function to the window object, since it's being called from an onclick attribute:
+
+
+window.checkAnswer = checkAnswer;
